fa04fc61c8db9664559321b4ec26c5ab9034c420
Florian Peipe <florianpeipemoodle@gmail.com>
2026-04-18 09:58:54 +0200

    chore: add .npmrc + migrate to ESLint flat config

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+const expoConfig = require("eslint-config-expo/flat");
+const prettierConfig = require("eslint-config-prettier");
+const prettierPlugin = require("eslint-plugin-prettier");
+
+module.exports = [
+  ...expoConfig,
+  prettierConfig,
+  {
+    plugins: { prettier: prettierPlugin },
+    rules: {
+      "prettier/prettier": "warn",
+      "react-hooks/exhaustive-deps": "error",
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+    },
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      "@typescript-eslint/consistent-type-imports": ["warn", { prefer: "type-imports" }],
+    },
+  },
+  {
+    ignores: [
+      "node_modules/**",
+      ".expo/**",
+      "dist/**",
+      "coverage/**",
+      "android/**",
+      "ios/**",
+      "docs/**",
+    ],
+  },
+];
